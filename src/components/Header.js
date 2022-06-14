24e36db5c2351a1fd8e950e5d0a32944e8cb72f2
MishaelRatemo <ratemomishael@gmail.com>
2022-06-14 14:36:02 +0300

    Added neflix logo and avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,8 @@ import '../App.css';
 function Header() {
   return (
     <div className = "header">
-      <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' />
-      <img className='userAvatar'  src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/e70b1333850498.56ba69ac32ae3.png' />
+      <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png' alt='' />
+      <img className='userAvatar'  src='https://mir-s3-cdn-cf.behance.net/project_modules/disp/e70b1333850498.56ba69ac32ae3.png' alt='' />
     </div>
   )
 }
