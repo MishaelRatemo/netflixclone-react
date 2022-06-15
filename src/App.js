348f3c812204486c457cabb104f69b5a3c2aa7ab
MishaelRatemo <ratemomishael@gmail.com>
2022-06-15 15:24:21 +0300

    included the Movie scroller in the main app>> App js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header.js';
 import Banner from './components/Banner';
+import MovieScroller from './components/MovieScroller';
 // import requests from './components/APIrequests.js'
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
+      <MovieScroller 
+        largeImage
+        title='Neflix Originals'
+        img ='https://flxt.tmsimg.com/assets/p14166741_b_h8_ai.jpg'
+        
+      />
     </div>
   );
 }
