54462483f5e02eeaf6c1641536b9ccd7e0471a0b
MishaelRatemo <ratemomishael@gmail.com>
2022-06-23 14:17:05 +0300

    Updated genres in the main app

--- src/App.js
+++ src/App.js
@@ -4,17 +4,36 @@ import './App.css';
 import Header from './components/Header.js';
 import Banner from './components/Banner';
 import MovieScroller from './components/MovieScroller';
-// import requests from './components/APIrequests.js'
+import requests from './components/APIrequests.js'
 function App() {
   return (
     <div className="App">
       <Header />
       <Banner />
       <MovieScroller 
-        largeImage
+        largeRow = { true}
         title='Neflix Originals'
-        img ='https://flxt.tmsimg.com/assets/p14166741_b_h8_ai.jpg'
-        
+        fetchURL={requests.netflixOriginals}        
+      />
+       <MovieScroller 
+        largeRow = { false}
+        title='Trending Now'
+        fetchURL={requests.trendingNow}        
+      />
+       <MovieScroller 
+        largeRow = { false}
+        title='Top Rated'
+        fetchURL={requests.topRated}        
+      />
+       <MovieScroller 
+        largeRow = { false}
+        title='Action Movies'
+        fetchURL={requests.actionMovies}        
+      />
+       <MovieScroller 
+        largeRow = { false}
+        title='Romantic Movies'
+        fetchURL={requests.documentaries}        
       />
     </div>
   );
