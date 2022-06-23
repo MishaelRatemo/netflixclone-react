f12208991b4c65bf6602e841e1cdb6ad5ccc5c7c
MishaelRatemo <ratemomishael@gmail.com>
2022-06-23 14:15:59 +0300

    added functionality to ROWs with different genres

--- src/components/MovieScroller.js
+++ src/components/MovieScroller.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from '../axios'
+import './MovieScroller.css'
+const baseURL = 'https://image.tmdb.org/t/p/original/';
 
-function MovieScroller({ title, fetchURL, largeImage}) {
+function MovieScroller({ title, fetchURL, largeRow}) {
+
+  const[movies,setMovies] = useState([])
+  useEffect(() =>{
+      async function fetchData(){
+        const request = await axios.get(fetchURL);
+        setMovies(request.data.results);
+        console.log(request.data.results);
+        return request;
+      }
+      fetchData();
+  }, []);
   return (
-    <div className='scroll'>
+    <div className='row'>
         {title}
-        <div className='poster'>
-            <img src='https://flxt.tmsimg.com/assets/p14166741_b_h8_ai.jpg' alt='' />
+        <div className='row_posters'>
+          {movies.map(movie =>(
+              <img 
+                  key={movie.id}  
+                  className={`row_poster ${largeRow && 'row_posterLarge'}`}
+                  src={`${baseURL}${largeRow ? movie.poster_path : movie.backdrop_path}`} 
+                  alt={movie.name} />
+          ))}
+          
+           
         </div>
     </div>
   )
