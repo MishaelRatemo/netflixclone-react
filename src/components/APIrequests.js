ed4c8f1c63a322aa86adf96b21ef0ea2e1b47b65
MishaelRatemo <ratemomishael@gmail.com>
2022-06-23 14:15:04 +0300

    edited netflix original urls endpoint

--- src/components/APIrequests.js
+++ src/components/APIrequests.js
@@ -3,7 +3,7 @@ const APIKEY = '85459567afbba3a7094fe38bddbc8eb7'
 const requests ={
     trendingNow:`/trending/all/week?api_key=${APIKEY}&language=en-US`,
     topRated:`/movie/top_rated?api_key=${APIKEY}&language=en-US`,
-    netflixOriginals:`/discover/movie?api_key=${APIKEY}&with_networks=123`,
+    netflixOriginals:`/discover/tv?api_key=${APIKEY}&with_networks=123`,
     actionMovies:`/discover/movie?api_key=${APIKEY}&with_genre=28`,
     romanticMovies:`/discover/movie?api_key=${APIKEY}&wth_genre=10749`,
     comedies:`/discover/movie?api_key=${APIKEY}&with_genre=35`,
